8de55779f259a48682376684434f928b57b9b538
AndrewGPFranco <andrewgomes1328@gmail.com>
2025-09-01 22:35:05 -0300

    feat: iniciando adaptação da tela de registro

--- src/components/auth/FormRegister.vue
+++ src/components/auth/FormRegister.vue
@@ -1,5 +1,20 @@
 <template>
-  <div>
-    NAO IMPLEMENTADO AINDA
-  </div>
+  <section>
+
+  </section>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const rolesTypes = ["LEITOR", "ESCRITOS", "MODERADOR", "ADMINISTRADOR"];
+
+const nome = ref<string>('')
+const email = ref<string>('')
+const senha = ref<string>('')
+const roles = ref<string>('')
+const nomeCompleto = ref<string>('')
+const dataNascimento = ref<string>('')
+const identificador = ref<string>('')
+
+</script>
